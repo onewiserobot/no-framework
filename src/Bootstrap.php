1f55536bc23d5455df76741327b9b95fe1bc7b84
Chris Rodgers <chris.rodgers@axion.com.au>
2019-01-23 15:40:59 +1100

    Added error handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,2 +1,19 @@
 <?php declare(strict_types = 1);
-echo 'Hello World';
+namespace Example;
+require __DIR__ . '/../vendor/autoload.php';
+error_reporting(E_ALL);
+$environment = 'development';
+/**
+* Register the error handler
+*/
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+  $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+  $whoops->pushHandler(function ($e) {
+    echo 'TODO: friendly error page and send email to the developer';
+  });
+}
+$whoops->register();
+
+throw new \Exception;
